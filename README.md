f54e4b3caab46287be95fdf3592140df227358af
Philipp Bauer <hello@philippbauer.org>
2019-12-15 15:32:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+lambda-performance-tuner
+========================
+
+Automatically determines the best cost / performance balance for an AWS Lambda function.
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/lambda-performance-tuner.svg)](https://npmjs.org/package/lambda-performance-tuner)
+[![Downloads/week](https://img.shields.io/npm/dw/lambda-performance-tuner.svg)](https://npmjs.org/package/lambda-performance-tuner)
+[![License](https://img.shields.io/npm/l/lambda-performance-tuner.svg)](https://github.com/philippjbauer/lambda-performance-tuner/blob/master/package.json)
+
+<!-- toc -->
+# Setup
+
+Log in to your AWS account and create a new policy with the following configuration, call it **LambdaPerformanceTunerAccess**.
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "LambdaPerformanceTuner",
+            "Effect": "Allow",
+            "Action": [
+                "lambda:ListFunctions",
+                "lambda:InvokeFunction",
+                "lambda:UpdateFunctionConfiguration",
+                "lambda:GetFunctionConfiguration"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+Create a user with **Programmatic access** called **LambdaPerformanceTuner** and attach the policy to the user. Take note of both the **access key ID** and **secret access key**.
+
+Add a new profile **lambdatuner** (or name of your choosing) to your `~/.aws/credentials` file like in the example below. You can use the `-p "my-profile"` option to switch between profiles.
+
+```ini
+# ~/.aws/credentials
+[lambdatuner]
+aws_access_key_id=AKIA***********LSGNX
+aws_secret_access_key=fJSuPKDx************************UxSwILfW
+```
+
+Add a new profile **lambdatuner** (or name of your choosing) to your `~/.aws/config` file like in the example below.
+
+```ini
+# ~/.aws/config
+[lambdatuner]
+region=us-east-1
+output=json
+```
+
+You can run `npm link` in this directory to install the CLI globally and get access to the CLI command `lamda-tuner`!
+
+# Usage
+<!-- usage -->
+
+To run execute `./bin/run` or `lambda-tuner` in your command line. To get help execute `./bin/run -h` or `lambda-tuner -h`.
---
+++ src/index.ts
@@ -0,0 +1,211 @@
+import { Command, flags } from '@oclif/command'
+import * as AWS from 'aws-sdk'
+import Cli from 'cli-ux'
+import Chalk from 'chalk'
+import Inquirer, { Answers } from 'inquirer'
+
+const iniLoader: AWS.IniLoader = new AWS.IniLoader()
+
+class LambdaPerformanceTuner extends Command {
+  static description = 'Automatically determines the best cost / performance balance for an AWS Lambda function.'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+    region: flags.string({char: 'r', default: 'us-east-1', description: 'AWS region your Lambda function lives in.'}),
+    profile: flags.string({char: 'p', default: 'lambdatuner', description: 'Local profile of the AWS user to use.'}),
+    list: flags.boolean({char: 'l', description: 'List all available Lambda functions.'}),
+    min: flags.integer({char: 'm', default: 128, description: 'Minimum amount of RAM to test for Lambda function.'}),
+    max: flags.integer({char: 'M', default: 1024, description: 'Maximum amount of RAM to test for Lambda function.'})
+  }
+
+  lambda: (AWS.Lambda | undefined)
+
+  async run() {
+    const {args, flags} = this.parse(LambdaPerformanceTuner)
+
+    try {
+      this.awsSetup(flags.profile, flags.region)
+
+      // Run List CLI Option
+      if (flags.list) {
+        await this.runList()
+        return
+      }
+
+      await this.runTuner()
+    } catch (error) {
+      this.customError(error)
+    }
+  }
+
+  /**
+   * Run List CLI Option
+   * 
+   * Retrieves the Lambda functions of the set region
+   * and displays them in the CLI.
+   * 
+   * @returns {Promise<void>}
+   * @memberof LambdaPerformanceTuner
+   */
+  async runList(): Promise<void> {
+    const functions: LambdaFunctionInformation[] = await this.listFunctions()
+
+    Cli.log(`\n`)
+    Cli.table(functions, {
+      functionName: {header: 'Name'},
+      memorySize: {
+        header: 'Memory',
+        get: (row) => Chalk`{${this.getMemoryColor(row.memorySize)}.bold ${row.memorySize}MB}`
+      },
+      state: {header: 'State'}
+    })
+  }
+
+  /**
+   * Run Tuning CLI Option
+   * 
+   * Asks the user which functions to tune and processes
+   * the selected functions with the help tuning of our
+   * tuning algorithm.
+   *
+   * @returns {Promise<void>}
+   * @memberof LambdaPerformanceTuner
+   */
+  async runTuner(): Promise<void> {
+    const functions: LambdaFunctionInformation[] = await this.listFunctions()
+
+    const answers: Answers = await Inquirer.prompt({
+        type: 'checkbox',
+        name: 'functions',
+        message: 'Please select a Lambda to tune:',
+        choices: functions.map((func: LambdaFunctionInformation) => {
+          const functionName: string = func.functionName.length > 80
+            ? `${func.functionName.substr(0, 79)}…`
+            : func.functionName
+          
+          const name: string = Chalk`{${this.getMemoryColor(func.memorySize)}.bold ${func.memorySize.toString().padStart(4, ' ')}MB} ${functionName}`
+          const value: string = func.functionName
+          
+          return {name, value}
+        })
+      })
+    
+    console.log(answers)
+    
+    // Create tuning algorithm and process all selected functions
+  }
+
+  /**
+   * List Lambda Functions
+   * 
+   * Uses AWS Lambda Service to retrieve the current user's
+   * Lambda functions and returns their relevant information
+   * we use to display to the user in the CLI.
+   *
+   * @returns {Promise<LambdaFunctionInformation[]>}
+   * @memberof LambdaPerformanceTuner
+   */
+  async listFunctions(): Promise<LambdaFunctionInformation[]> {
+    let functions: LambdaFunctionInformation[] = []
+
+    Cli.action.start(`Retrieving your Lambda functions.`)
+    
+    const listFunctionsResponse : AWS.Lambda.ListFunctionsResponse | undefined = 
+      await this.lambda?.listFunctions().promise()
+    
+    if (!listFunctionsResponse?.Functions) {
+      throw new Error(`Functions undefined in Lambda's ListFunctionsResponse.`);
+    }
+    
+    functions = listFunctionsResponse.Functions.map((func) => {
+      return {
+        functionName: func.FunctionName,
+        description: func.Description,
+        memorySize: func.MemorySize,
+        runtime: func.Runtime,
+        state: func.State || 'Unknown'
+      } as LambdaFunctionInformation
+    })
+    
+    if (functions.length === 0) {
+      throw new Error(`No AWS Lambda functions found in this region.`)
+    }
+
+    Cli.action.stop(Chalk`{green.bold ${functions.length} function(s) found. }`)
+
+    return functions
+  }
+
+  /**
+   * AWS Setup
+   * 
+   * Create the necessary environment for the CLI to run.
+   *
+   * @param {string} [profile='lambdatuner']
+   * @param {string} [region='us-east-1']
+   * @memberof LambdaPerformanceTuner
+   */
+  awsSetup(profile: string = 'lambdatuner', region: string = 'us-east-1'): void {
+    const credentials = iniLoader.loadFrom({})
+    const config = iniLoader.loadFrom({isConfig: true})
+    const ini = Object.assign({}, credentials[profile], config[profile]);
+
+    // Configure Lambda Service
+    this.lambda = new AWS.Lambda({
+      apiVersion: '2015-03-31',
+      accessKeyId: ini.aws_access_key_id,
+      secretAccessKey: ini.aws_secret_access_key,
+      region: ini.region
+    })
+    
+    Cli.log(Chalk`Using AWS Region: {green.bold ${this.lambda.config.region}}`)
+  }
+
+  /**
+   * Custom Error Handler
+   * 
+   * Display a nicer error than the default oclif error at `this.error`.
+   *
+   * @param {(string | Error)} input
+   * @param {{
+   *       code?: string;
+   *       exit?: number;
+   *   }} [options]
+   * @returns {never}
+   * @memberof LambdaPerformanceTuner
+   */
+  customError(input: string | Error, options?: {
+      code?: string;
+      exit?: number;
+  }): never {
+    const name = input instanceof Error ? input.name : 'Error'
+    const message = input instanceof Error ? input.message : input
+
+    Cli.log(Chalk`{red.bold ${name}${options?.code ? ` {bgRed.white ${options.code}}` : ''}:} {red ${message}}`)
+    Cli.exit(options?.exit);
+  };
+
+  getMemoryColor(size: number): string {
+    return size > 1024
+      ? 'red'
+      : size > 512
+        ? 'yellow'
+        : 'green'
+  }
+}
+
+interface LambdaFunctionInformation {
+  functionName: string
+  description?: string
+  memorySize: number
+  runtime: string
+  state: string
+}
+
+interface LambdaFunctionSettings {
+  memorySize: number
+}
+
+export = LambdaPerformanceTuner
